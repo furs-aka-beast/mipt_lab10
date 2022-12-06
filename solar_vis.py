02915d49ca57b52cdb31be6c848d51f40f77a3ec
furs-aka-beast <kiruha200409@gmail.com>
2022-12-06 17:43:26 +0300

    fixed x calc

--- solar_vis.py
+++ solar_vis.py
@@ -39,7 +39,7 @@ def scale_x(x):
     **x** — x-координата модели.
     """
 
-    return int(x*scale_factor) + window_width//2
+    return int(x*scale_factor) + int(window_width//2)
 
 
 def scale_y(y):
@@ -54,7 +54,7 @@ def scale_y(y):
     **y** — y-координата модели.
     """
 
-    return int(y*scale_factor) + window_width//2
+    return int(y*scale_factor) + int(window_height//2)
 
 
 def create_star_image(space, star):
